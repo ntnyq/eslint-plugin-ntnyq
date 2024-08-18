7ee593b8a0a2276d8937e3032bb144ca9c153797
ntnyq <ntnyq13@gmail.com>
2024-08-18 16:33:38 +0800

    test: tweak test cases indent

--- tests/rules/no-member-accessibility.test.ts
+++ tests/rules/no-member-accessibility.test.ts
@@ -1,136 +1,136 @@
-import { run } from '../internal'
+import { $, run } from '../internal'
 import rule, { RULE_NAME, messageId } from '../../src/rules/no-member-accessibility'
 
 const validCases = [
-  `
-class Test {
-  x: number
-  constructor(x: number) {
-    this.x = x
-  }
-  get internalValue() {
-    return this.x
-  }
-  set internalValue(value: number) {
-    this.x = value
-  }
-  square(): number {
-    return this.x * this.x
-  }
-  half(): number {
-    return this.x / 2
-  }
-}
-`,
+  $`
+    class Test {
+      x: number
+      constructor(x: number) {
+        this.x = x
+      }
+      get internalValue() {
+        return this.x
+      }
+      set internalValue(value: number) {
+        this.x = value
+      }
+      square(): number {
+        return this.x * this.x
+      }
+      half(): number {
+        return this.x / 2
+      }
+    }
+  `,
 ]
 const invalidCases = [
   [
-    `
-class Test {
-  public static internalValue() {
-    return this.x
-  }
-}
-`,
-    `
-class Test {
-  static internalValue() {
-    return this.x
-  }
-}
-`,
+    $`
+      class Test {
+        public static internalValue() {
+          return this.x
+        }
+      }
+    `,
+    $`
+      class Test {
+        static internalValue() {
+          return this.x
+        }
+      }
+    `,
   ],
   [
-    `
-class Test {
-  private x: number
-}
-`,
-    `
-class Test {
-  x: number
-}
-`,
+    $`
+      class Test {
+        private x: number
+      }
+    `,
+    $`
+      class Test {
+        x: number
+      }
+    `,
   ],
   [
-    `
-class Test {
-  public get internalValue() {
-    return this.x
-  }
-}
-`,
-    `
-class Test {
-  get internalValue() {
-    return this.x
-  }
-}
-`,
+    $`
+      class Test {
+        public get internalValue() {
+          return this.x
+        }
+      }
+    `,
+    $`
+      class Test {
+        get internalValue() {
+          return this.x
+        }
+      }
+    `,
   ],
   [
-    `
-class Test {
-  public set internalValue(value: number) {
-    this.x = value
-  }
-}
-`,
-    `
-class Test {
-  set internalValue(value: number) {
-    this.x = value
-  }
-}
-`,
+    $`
+      class Test {
+        public set internalValue(value: number) {
+          this.x = value
+        }
+      }
+    `,
+    $`
+      class Test {
+        set internalValue(value: number) {
+          this.x = value
+        }
+      }
+    `,
   ],
   [
-    `
-class Test {
-  public square(): number {
-    return this.x * this.x
-  }
-}
-`,
-    `
-class Test {
-  square(): number {
-    return this.x * this.x
-  }
-}
-`,
+    $`
+      class Test {
+        public square(): number {
+          return this.x * this.x
+        }
+      }
+    `,
+    $`
+      class Test {
+        square(): number {
+          return this.x * this.x
+        }
+      }
+    `,
   ],
   [
-    `
-class Test {
-  protected half(): number {
-    return this.x / 2
-  }
-}
-`,
-    `
-class Test {
-  half(): number {
-    return this.x / 2
-  }
-}
-`,
+    $`
+      class Test {
+        protected half(): number {
+          return this.x / 2
+        }
+      }
+    `,
+    $`
+      class Test {
+        half(): number {
+          return this.x / 2
+        }
+      }
+    `,
   ],
   [
-    `
-class Test {
-  public constructor(x: number) {
-    this.x = x
-  }
-}
-`,
-    `
-class Test {
-  constructor(x: number) {
-    this.x = x
-  }
-}
-`,
+    $`
+      class Test {
+        public constructor(x: number) {
+          this.x = x
+        }
+      }
+    `,
+    $`
+      class Test {
+        constructor(x: number) {
+          this.x = x
+        }
+      }
+    `,
   ],
 ]
 
