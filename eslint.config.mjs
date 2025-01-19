fe31c9d07e30f7c652aa8517b4cabaf7895cf555
ntnyq <ntnyq13@gmail.com>
2025-01-19 12:06:38 +0800

    chore: update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,8 @@
 // @ts-check
 
 import { defineESLintConfig } from '@ntnyq/eslint-config'
-import pluginESLintPlugin from 'eslint-plugin-eslint-plugin'
 
-const config = defineESLintConfig(
-  {
-    ignores: ['**/tests/fixtures'],
-  },
-  [
-    {
-      ...pluginESLintPlugin.configs['flat/all'],
-      rules: {
-        ...pluginESLintPlugin.configs['flat/all'].rules,
-        'eslint-plugin/require-meta-docs-url': 'off',
-      },
-    },
-  ],
-)
-
-export default config
+export default defineESLintConfig({
+  eslintPlugin: true,
+  ignores: ['**/tests/fixtures'],
+})
