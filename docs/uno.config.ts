318c4a90d62a934108e8766f951106757d7a4e9c
ntnyq <ntnyq13@gmail.com>
2025-02-25 22:59:19 +0800

    chore: update

--- docs/uno.config.ts
+++ docs/uno.config.ts
@@ -1,12 +1,35 @@
-import { defineConfig, presetIcons, presetUno } from 'unocss'
+import {
+  defineConfig,
+  presetIcons,
+  presetWind3,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
 
 export default defineConfig({
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+
   presets: [
-    presetUno(),
+    presetWind3(),
     presetIcons({
       autoInstall: true,
       extraProperties: {},
       scale: 1.2,
     }),
   ],
+
+  shortcuts: [
+    {
+      'flex-center': 'flex justify-center items-center',
+      'wh-full': 'w-full h-full',
+    },
+  ],
+
+  theme: {
+    colors: {
+      // primary: {
+      //   DEFAULT: '',
+      // },
+    },
+  },
 })
